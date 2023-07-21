ee1877cb11a4930771f0a1ef4ae1ef293fe416b2
hetmanjones <hetman.jones@protonmail.com>
2023-07-21 20:05:42 +0200

    keep progressing

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import { DeploymentConfig } from "../types";
 
 export const CONFIG: DeploymentConfig = {
-    marketMakerAddress: "",
-    sampleUserAddress: ""
+    marketMakerAddress: "0xEB40824601A19BdF20542a677E52f1E4297c66Fa", // account 2
+    sampleUserAddress: "0xC12AE746678740A052B60c3D6b591ed7ADee5926" // account 3
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
@@ -17,14 +19,41 @@ const config: HardhatUserConfig = {
     deployer: 0
   },
   networks: {
-    // hardhat: {
-    // },
-    // arbitrum_goerli: {
-
-    // },
-    // goerli: {
-
-    // }
+    hardhat: {
+      forking: {
+        url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+        enabled: true,
+        blockNumber: 113544548
+      }
+    },
+    network1: {
+      url: "http://localhost:8545",
+      forking: {
+        url: `https://arbitrum-goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+        enabled: true
+      }
+    },
+    network2: {
+      url: "http://localhost:8546",
+      forking: {
+        url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+        enabled: true
+      }
+    },
+    arbitrum_goerli: {
+      url: `https://arbitrum-goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+      accounts: {
+        mnemonic: process.env.ARBITRUM_TESTNET_MNEMONIC || "",
+        initialIndex: 1
+      }
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY || ""}`,
+      accounts: {
+        mnemonic: process.env.GOERLI_TESTNET_MNEMONIC || "",
+        initialIndex: 2
+      }
+    }
   }
 };
 
